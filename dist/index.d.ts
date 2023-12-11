a0b430c78c26a9ae73a9883b2d54fbef6da135b7
y1j2x34 <y1j2x34@qq.com>
2023-12-11 17:03:33 +0800

    feat: release 1.0.6

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,9 +2,11 @@ import { ApplicationContext } from '@vgerbot/ioc';
 import React from 'react';
 import { Newable } from '@vgerbot/ioc/dist/types/Newable';
 export declare function Observable(): Function;
+export declare function AutoObservable(): Function;
 export type IoCProps = React.PropsWithChildren<{
     callback?: (appctx: ApplicationContext) => void;
 }>;
 export declare function IoC(props: IoCProps): React.FunctionComponentElement<React.ProviderProps<ApplicationContext | undefined>>;
 export declare function useService<T>(ctor: Newable<T>): T;
+export declare function renderObserver(render: () => React.ReactElement): React.ReactElement<any, string | React.JSXElementConstructor<any>>;
 export * from '@vgerbot/ioc';
